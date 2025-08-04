4eda4b185aad1499f56ba7722259316be5d727c7
TwelveDev <ywytleongtan@gmail.com>
2025-08-04 16:11:29 +0800

    _

--- BCMS1013 asg codebase/FileName.cpp
+++ BCMS1013 asg codebase/FileName.cpp
@@ -4,23 +4,7 @@
 using namespace std;
 
 int main() {
-	const int row = 5, col = 8;
-	int date_day[5][8] = { 
-		{1, 2, 3, 4, 5, 6, 7},
-		{8, 9, 10, 11, 12, 13, 14},
-		{15, 16, 17, 18, 19, 20, 21},
-		{22, 23, 24, 25, 26, 27, 28},
-		{29, 30, 31, 0, 0, 0, 0, 0} 
-	};
-	for (int i = 0; i < row; ++i) {
-		for (int j = 0; j < col; ++j) {
-			if (date_day[i][j] != 0)
-				cout << date_day[i][j] << " ";
-			if (date_day[i][j] == 12)
-				cout << "\033[47m" << date_day[i][j] << "\033[0m";
-		}
-		cout << endl;
-	}
+
 	return 0;
 }
 
@@ -104,6 +88,23 @@ void payment(){
 }
 void displayCalendar(){
 	//display the calendar & highlights days that our experts have slots for customer to view
+	const int row = 5, col = 8;
+	int date_day[5][8] = {
+		{1, 2, 3, 4, 5, 6, 7},
+		{8, 9, 10, 11, 12, 13, 14},
+		{15, 16, 17, 18, 19, 20, 21},
+		{22, 23, 24, 25, 26, 27, 28},
+		{29, 30, 31, 0, 0, 0, 0, 0}
+	};
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < col; ++j) {
+			if (date_day[i][j] != 0)
+				cout << date_day[i][j] << " ";
+			if (date_day[i][j] == 12)
+				cout << "\033[47m" << date_day[i][j] << "\033[0m";
+		}
+		cout << endl;
+	}
 }
 void viewbookedSchedule(){
 }
