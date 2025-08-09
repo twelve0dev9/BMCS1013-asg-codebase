dcae0d61a3b1dab00105c03da99fdf7179693a36
TwelveDev <ywytleongtan@gmail.com>
2025-08-09 15:29:07 +0800

    update on viewAvailable_days function, dis time is if statement condition not met hence not performing the body, hence unexpected output

--- BCMS1013 asg codebase/test driver.cpp
+++ BCMS1013 asg codebase/test driver.cpp
@@ -49,24 +49,18 @@ void viewAvailable_days(int choice_expert, services services_available[],
 	cout << endl << endl;
 	cout << "total bookings : " << 62 << endl;
 	for (int k = 0; k < totalBookings; ++k) //goes over the bookings list iteratively
-	{  
-		cout << "Processing bookings #" << k << endl;
-		cout << "Date of " << k+1 << "th appointment : " << appointments_schedule[k].booking_date << endl;
+	{							//dis compares if the appointments' booked expert is the same as what user choosed
+																				//what the user has chosen depends on choice_expert var - 1
+		cout << "\nProcessing appointment " << k + 1 << "th\n";
 		int date = appointments_schedule[k].booking_date;
-		cout << "bookingsPerDay accumulator value at " << date << "th array before increment : " << bookingsPerDay[date] << endl;
 		bookingsPerDay[date]++;
-		cout << "bookingsPerDay accumulator value at " << date << "th array after increment : " << bookingsPerDay[date] << endl;
-		cout << "\n_\n_\n_" /*<< bookingsPerDay[date] */<< "\n_\n_\n";
-		//if (appointments_schedule[k].expert_booked->userID == experts[choice_expert].userID && appointments_schedule[k].booking_status == true)
-		//{
-		//	int date = appointments_schedule[k].booking_date;
-		//	if (date >= 1 && date <= 31)
-		//	{
-		//		bookingsPerDay[date]++;
-		//		cout << appointments_schedule[k].booking_date;
-		//		cout << "\n_\n_\n_" << bookingsPerDay[date] << "\n_\n_\n";
-		//	}
-		//}
+		if (appointments_schedule[k].expert_booked->userID == experts[choice_expert + 1].userID && appointments_schedule[k].booking_status == true)
+		{
+			if (date >= 1 && date <= 31)
+			{
+				cout << "is in if statement\n";
+			}
+		}
 	}
 	cout << setw(23) << "December" << endl
 		<< "-------------------------------------\n";
@@ -213,13 +207,13 @@ int main() {
 		{&hourly_timeSlots[4], true, 14, &customer_users[6], &experts[3], &services_available[2]},
 		{&hourly_timeSlots[1], true, 28, &customer_users[7], &experts[2], &services_available[3]}
 	};
+	bookings* P_appointments_schedule = appointments_schedule;
 	int totalBookings = sizeof(appointments_schedule) / sizeof(appointments_schedule)[0];
 			//get the size of appointments_schedule, get the size of the whole array
 			//get the size of one element only in appointments_scehdule, get the size of one element only in our array
 			//dividing 'em gets the number of elements we have in our appointments_schedule()
 			//putting into context means the number of bookings we have
 			//this integer we later need to pass to void viewAvailable_days() for processings
-	bookings* P_appointments_schedule = appointments_schedule;
 	//for (int i = 0; i < 2; ++i) {
 	//	bookings& appointments = appointments_schedule[i];
 
