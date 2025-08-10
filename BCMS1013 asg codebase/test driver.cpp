1f8f92d49cf34cb4fecaa66f539367d5b40babeb
TwelveDev <ywytleongtan@gmail.com>
2025-08-11 00:45:50 +0800

    working on alnum checking

--- BCMS1013 asg codebase/test driver.cpp
+++ BCMS1013 asg codebase/test driver.cpp
@@ -1,58 +1,31 @@
-//#include<iostream>
-//#include<iomanip>
-//#include<string>
-//using namespace std;
-//
-//enum UserType { admin, expert, customer };
-//struct services {
-//	int serviceID = 0;
-//	string service_name = " ";
-//	float serviceTime = 0.00; //unit is Hours
-//};
-//struct users {//the users records might put under sum kind of function 
-//	//no point in importing the user records from the file globally when not in use
-//	int userID = 0;
-//	string username = " ";
-//	int age = 0;
-//	char gender = ' ';
-//	string user_email = " ";
-//	string user_password = " ";
-//	UserType user_Type;
-//	services* specialization[2];
-//};
-//void niga(users experts1[]);
-//void niga2(users experts2[]);
-//int main() {
-//	int choice_expert = 0;
-//	services services_available[4] = {
-//		{1, "Haircuts & Trims", 3},
-//		{2, "Bread Grooming", 3},
-//		{3, "Facial Skin Care", 3},
-//		{4, "Massage Therapy", 3}
-//	};
-//	users experts[] = {
-//		{29, "Aina", 24, 'F', "aina2312@gmail.com", "passwordbruh102", expert, {&services_available[0], &services_available[2]}},
-//		{3, "Hitler", 23, 'M', "sashimidelicious@gmail.com", "anitam4xw8n", expert, {&services_available[1], &services_available[3]}},
-//		{41, "John", 34, 'M', "johnwaynecas@gmail.com", "apovusbg876trds9", expert, {&services_available[1], &services_available[2]}},
-//		{42, "Beyonce", 34, 'F', "diddyparty@gmail.com", "nobabyoil", expert, {&services_available[2], &services_available[3]}},
-//		{10, "Hela", 30, 'M', "helathor@gmail.com", "oyud6759iu41", expert, {&services_available[0], &services_available[1]}}
-//	};
-//	users* pExpert = experts;
-//	cout << sizeof(*pExpert);
-//	niga(experts);
-//	return 0;
-//}
-//void niga(users experts1[]) {
-//	for (int i = 0; i < sizeof(*experts1) / sizeof(experts1)[0]; ++i)
-//	{
-//		cout << experts1[i].userID << ' ' << experts1[i].username << endl;
-//		cout << sizeof(*experts1) << "+++" << sizeof(experts1)[0] << endl;
-//	}
-//	niga2(experts1);
-//}
-//void niga2(users experts2[]) {
-//	for (int i = 0; i < sizeof(*experts2) / sizeof(experts2)[0]; ++i)
-//	{
-//		cout << experts2[i].userID << ' ' << experts2[i].username << endl;
-//	}
-//}
+#include<iostream>
+#include<iomanip>
+#include<string>
+#include<cstdlib>
+using namespace std;
+
+bool checkAlpha(const string& stringVar)
+{ // validates if the input provided is alphenumeric or not similar to that of Python
+	cout << "Function called .... \n";
+	bool yesalnum = 0;
+	for (char ch : stringVar) {
+		if (isdigit(static_cast<unsigned>(ch)))
+		{
+			cout << "true";
+			yesalnum = 1;
+		}
+	}
+	return yesalnum;
+}
+int main() {
+	int choice_menu;
+	cout << "1. View our serivces\n2. Book an appointment\n3. View booked schedule\n4. Exit\n\n";
+	cin >> choice_menu;
+	cout << checkAlpha(to_string(choice_menu));
+	//while (choice_menu < 1 || choice_menu > 4 || checkAlpha(to_string(choice_menu)) == false)
+	//{
+	//	cout << "Enter valid choice !! : ";
+	//	cin >> choice_menu;
+	//}
+	return 0;
+}
