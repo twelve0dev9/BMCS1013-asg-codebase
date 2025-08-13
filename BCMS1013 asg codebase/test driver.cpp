bdc0d7a6a7a9dd9ece0fd65166f8b404827bb7f4
TwelveDev <ywytleongtan@gmail.com>
2025-08-13 11:46:16 +0800

    clean up the test driver a bit 1

--- BCMS1013 asg codebase/test driver.cpp
+++ BCMS1013 asg codebase/test driver.cpp
@@ -29,8 +29,6 @@ int main() {
 	string choice_menuu;
 	cout << "1. View our serivces\n2. Book an appointment\n3. View booked schedule\n4. Exit\n\n";
 	cin >> choice_menuu;
-	//cout << checkAlpha(to_string(choice_menu));
-	cout << isAlphabet(choice_menuu.c_str()) << endl;
 	while (isAlphabet(choice_menuu.c_str()) == true || 
 		isAlphaNum(choice_menuu.c_str()) == false || 
 		stoi(choice_menuu) < 1 || stoi(choice_menuu) > 4)
