57c3e9727a0d9952a1861df7c69097c7cbf1014e
twelvedev <ywytleongtan@gmail.com>
2025-08-13 12:29:48 +0800

    integrate validation check functions into FileName.cpp

--- BCMS1013 asg codebase/test driver.cpp
+++ BCMS1013 asg codebase/test driver.cpp
@@ -1,40 +1,40 @@
-#include<iostream>
-#include<iomanip>
-#include<string>
-#include<cstdlib>
-using namespace std;
-
-bool isAlphabet(const char* str) // to check if the input is Alphabet or not
-{
-	for (int i = 0; str[i] != '\0'; ++i) 
-	{
-		if (!isalpha(str[i])) {
-			return false;
-		}
-	}
-	return true;
-}
-bool isAlphaNum(const char* str) // to check if the input has *&%(*& symbols, non-alphabet, & non-numeric
-{
-	for (int i = 0; str[i] != '\0'; ++i) 
-	{
-		if (!isalnum(str[i])) {
-			return false;
-		}
-	}
-	return true;
-}
-int main() {
-	int choice_menu;
-	string choice_menuu;
-	cout << "1. View our serivces\n2. Book an appointment\n3. View booked schedule\n4. Exit\n\n";
-	cin >> choice_menuu;
-	while (isAlphabet(choice_menuu.c_str()) == true || 
-		isAlphaNum(choice_menuu.c_str()) == false || 
-		stoi(choice_menuu) < 1 || stoi(choice_menuu) > 4)
-	{
-		cout << "Enter valid choice !! : ";
-		cin >> choice_menuu;
-	}
-	return 0;
-}
+//#include<iostream>
+//#include<iomanip>
+//#include<string>
+//#include<cstdlib>
+//using namespace std;
+//
+//bool isAlphabet(const char* str) // to check if the input is Alphabet or not
+//{
+//	for (int i = 0; str[i] != '\0'; ++i) 
+//	{
+//		if (!isalpha(str[i])) {
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//bool isAlphaNum(const char* str) // to check if the input has *&%(*& symbols, non-alphabet, & non-numeric
+//{
+//	for (int i = 0; str[i] != '\0'; ++i) 
+//	{
+//		if (!isalnum(str[i])) {
+//			return false;
+//		}
+//	}
+//	return true;
+//}
+//int main() {
+//	int choice_menu;
+//	string choice_menuu;
+//	cout << "1. View our serivces\n2. Book an appointment\n3. View booked schedule\n4. Exit\n\n";
+//	cin >> choice_menuu;
+//	while (isAlphabet(choice_menuu.c_str()) == true || 
+//		isAlphaNum(choice_menuu.c_str()) == false || 
+//		stoi(choice_menuu) < 1 || stoi(choice_menuu) > 4)
+//	{
+//		cout << "Enter valid choice !! : ";
+//		cin >> choice_menuu;
+//	}
+//	return 0;
+//}
