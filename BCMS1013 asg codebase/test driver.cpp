74921976d0b657bff3f89a825852a424273b8069
TwelveDev <ywytleongtan@gmail.com>
2025-08-15 19:53:06 +0800

    input validation in progress 3

--- BCMS1013 asg codebase/test driver.cpp
+++ BCMS1013 asg codebase/test driver.cpp
@@ -15,18 +15,41 @@ bool isAlphaNum(const char* stringVar) // to check if the input has *&%(*& symbo
 {
 	for (int i = 0; stringVar[i] != '\0'; ++i) {
 		if (!isalnum(stringVar[i]))
-			return false;}
+	 		return false;}
 	return true;
+}
+int convertNumeric(const char* stringVar) {
+	int choice = stoi(stringVar);
+	return choice;
 }
 int main()
 {
-	int n = 0; 
-	cin >> n;
-	cout << isAlphabet(to_string(n).c_str());
-	//while (isAlphabet(cstr) == true) {
-	//	cout << "Invalid input, integer only.\n";
-	//	cin >> n;
-	//}
+	string choice = " ";
+	getline(cin, choice);
+	cout << isAlphabet(choice.c_str()) << endl << isAlphaNum(choice.c_str()) << endl
+		/*<< convertNumeric(choice.c_str()) << endl*/;
+	while (true) {
+		int choice1;
+		
+		if ( choice.length() == 1 && isAlphabet(choice.c_str()) == false )
+			choice1 = atoi(choice.c_str());
+		else
+			cout << "unable to convert\n";
+
+		if (   isAlphabet(choice.c_str()) == false
+			|| isAlphaNum(choice.c_str()) == false
+			|| choice1 < 1 || choice1 > 4 ) {
+			cout << "valid input\n";
+			break;
+		}
+		else {
+			cout << "Invalid input. Please enter integer only, within 1-4, don't provide alphabets or symbols.\n";
+			cin >> choice;
+		}
+	}
+	/*while (isAlphabet(choice.c_str()) == true || isAlphaNum(choice.c_str()) == false
+		|| convertNumeric(choice.c_str()) < 1 || convertNumeric(choice.c_str()) > 4) {
+	}*/
 	//while (true) {
 	//	cin >> n;
 	//	if (cin.fail()) {
@@ -37,7 +60,7 @@ int main()
 	//	} else
 	//		break;
 	//}
-	cout << endl << n;
+	cout << endl << choice;
 
 	return 0;
 }
